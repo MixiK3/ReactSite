8d9a16971c8696a72b21edf4a449d1a8314ba7ab
Mixik <mixik2005@mail.ru>
2024-05-15 23:25:06 +0300

    3 module

--- reactsite/src/main.tsx
+++ reactsite/src/main.tsx
@@ -1,17 +1,11 @@
-import React from 'react'
-import Artem from './pages/Artem'
-import Vlad from './pages/Vlad'
-import {
-  ARTEM_ROUTE,
-  GENA_ROUTE,
-  OLEG_ROUTE,
-  VLAD_ROUTE,
-  FORM_ROUTE,
-} from './App/routing/config'
-import { useRoutes } from 'react-router-dom'
-import Oleg from './pages/Oleg'
-import Gena from './pages/Gena'
-import Form from './pages/Form'
+import React from "react";
+import Artem from "./pages/Artem";
+import Vlad from "./pages/Vlad";
+import { ARTEM_ROUTE, GENA_ROUTE, OLEG_ROUTE, VLAD_ROUTE, FORM_ROUTE } from "./App/routing/config";
+import { useRoutes } from "react-router-dom";
+import Oleg from "./pages/Oleg";
+import Gena from "./pages/Gena";
+import Form from "./pages/Form";
 
 const MainRouter = () => {
   return useRoutes([
@@ -23,7 +17,7 @@ const MainRouter = () => {
     { path: OLEG_ROUTE, element: <Oleg /> },
     { path: GENA_ROUTE, element: <Gena /> },
     { path: FORM_ROUTE, element: <Form /> },
-  ])
-}
+  ]);
+};
 
-export default MainRouter
+export default MainRouter;
--- reactsite/src/pages/Artem/index.tsx
+++ reactsite/src/pages/Artem/index.tsx
@@ -1,67 +1,64 @@
-import { FC, useEffect, useState } from 'react'
-import CardUniversity, { IUniversity } from '../../components/CardUniversity'
-import { useInView } from 'react-intersection-observer'
-import axios from 'axios'
-import styled from 'styled-components'
+import { FC, useEffect, useState } from "react";
+import CardUniversity, { IUniversity } from "../../components/CardUniversity";
+import { useInView } from "react-intersection-observer";
+import axios from "axios";
+import styled from "styled-components";
 
-const LIMIT_UNIVERSITIES = 10
+const LIMIT_UNIVERSITIES = 10;
 
 const ListStyled = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
-`
+`;
 
 const BlockObserver = styled.div`
   height: 40px;
   background-color: black;
-`
+`;
 
 const DynamicPagination: FC = () => {
-  const [universities, setUniversities] = useState<Array<IUniversity>>([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [loading, setLoading] = useState(false)
+  const [universities, setUniversities] = useState<Array<IUniversity>>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetchUniversities()
-  }, [currentPage])
+    fetchUniversities();
+  }, [currentPage]);
 
   const { ref, inView } = useInView({
     threshold: 1.0,
-  })
+  });
 
   useEffect(() => {
     if (inView) {
-      setCurrentPage((prev) => prev + 1)
+      setCurrentPage((prev) => prev + 1);
     }
-  }, [inView])
+  }, [inView]);
 
   const fetchUniversities = async () => {
     try {
-      const offset = (currentPage - 1) * LIMIT_UNIVERSITIES
+      const offset = (currentPage - 1) * LIMIT_UNIVERSITIES;
       const response = await axios.get(
-        `http://universities.hipolabs.com/search?offset=${offset}&limit=${LIMIT_UNIVERSITIES}`
-      )
-      setUniversities((prev) => [...prev, ...response.data])
+        `http://universities.hipolabs.com/search?offset=${offset}&limit=${LIMIT_UNIVERSITIES}`,
+      );
+      setUniversities((prev) => [...prev, ...response.data]);
     } catch (error) {
-      console.log('Error fetching univer...', error)
+      console.log("Error fetching univer...", error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
   return (
     <ListStyled>
       <h1>List Universities</h1>
       {universities.map((university) => (
-        <CardUniversity
-          data={university}
-          key={university.name}
-        ></CardUniversity>
+        <CardUniversity data={university} key={university.name}></CardUniversity>
       ))}
       {loading && <div>Loading...</div>}
       {!loading && <BlockObserver ref={ref}></BlockObserver>}
     </ListStyled>
-  )
-}
+  );
+};
 
-export default DynamicPagination
+export default DynamicPagination;
